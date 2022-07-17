8110dc2139ab53711f5b9029ce1bd305dd265a8a
dobidugi <dobidugi@gmail.com>
2022-07-17 13:49:17 +0900

    :sparkles:: Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Global } from "@emotion/react";
 import React from "react";
+import Header from "./components/Header/Header";
+import DefaultStyle from "./DefaultStyle";
 
 function App() {
     return (
         <div>
-            test
+            <Global styles={DefaultStyle} />
+            <Header />
         </div>
     );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+import React from "react";
+
+function Header() {
+    return (
+        <header>Header</header>
+    );
+}
+
+export default Header;  
